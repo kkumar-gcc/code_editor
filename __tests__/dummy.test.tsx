fa4d04192880e3f9f706fbf2e237a142908ea5ab
kkumar-gcc <krishkumar9352@gmail.com>
2023-11-25 03:22:50 +0530

    remove empty test file

--- __tests__/dummy.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from "@testing-library/react";
-
-it('should have Dummy text', function () {
-    render(<div>Dummy</div>);
-
-    const myElement = screen.getByText(/Dummy/i);
-
-    expect(myElement).toBeInTheDocument();
-});
